fdc4bf3949349180b9a1ad38ca3307f0a58dfdfb
Vincent Cerri <vincentcerri@Vincents-MacBook-Pro.local>
2019-01-26 17:07:10 -0500

    header complete

--- ewaste/src/app/app-routing.module.ts
+++ ewaste/src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { SearchComponent } from './search/search.component';
+import { StartedComponent } from './started/started.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+
+
+const routes: Routes = [
+  { path: 'search', component: SearchComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'started', component: StartedComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
+
+
--- ewaste/src/app/app.module.ts
+++ ewaste/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { LoginComponent } from './login/login.component';
+import { SettingsComponent } from './settings/settings.component';
+import { SearchComponent } from './search/search.component';
+import { AboutComponent } from './about/about.component';
+import { StartedComponent } from './started/started.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    LoginComponent
+    LoginComponent,
+    SettingsComponent,
+    SearchComponent,
+    AboutComponent,
+    StartedComponent
   ],
   imports: [
     BrowserModule,
