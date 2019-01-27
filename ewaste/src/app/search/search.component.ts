35b3b7c8ca4e2217e5ee3d315d1a6e7cc3185ecf
Vincent Cerri <vincentcerri@Vincents-MacBook-Pro.local>
2019-01-27 07:59:46 -0500

    Changes to random

--- ewaste/src/app/search/search.component.ts
+++ ewaste/src/app/search/search.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {post} from 'selenium-webdriver/http';
+
+
 
 @Component({
   selector: 'app-search',
@@ -13,3 +18,27 @@ export class SearchComponent implements OnInit {
   }
 
 }
+
+//@Injectable()
+//  export class SearchComponent {
+//  constructor(private http: HttpClient) { }
+//}
+
+
+
+
+///** POST: search the database for a value */
+//searchRecord (record: Record): Observable<record> {
+
+//}
+
+
+/** POST: add a new hero to the database */
+//addHero (hero: Hero): Observable<Hero> {
+//  return this.http.post<Hero>(this.heroesUrl, hero, httpOptions)
+//    .pipe(
+//      catchError(this.handleError('addHero', hero))
+//    );
+//}
+
+
